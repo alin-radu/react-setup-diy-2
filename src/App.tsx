70bc07bafc2c862ee18ed693b0956533618b6487
Alin RADU <cristian.alin.radu@reea.net>
2022-12-30 16:37:33 +0200

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ClickCounter } from './ClickCounter';
+
 import './styles.css';
 
 import IMAGE from './react.png';
@@ -6,11 +8,12 @@ import LOGO from './logo.svg';
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>React TypeScript Webpack Starter Template, refreshed</h1>
       <h1>mode: {process.env.NODE_ENV}</h1>
       <h1>status: {process.env.name}</h1>
       <img src={IMAGE} alt="React logo" width="200" height="200" />
       <img src={LOGO} alt="React logo" width="200" />
+      <ClickCounter />
     </>
   );
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,14 +1,22 @@
 const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   mode: 'development',
 
   devtool: 'cheap-module-source-map',
 
+  devServer: {
+    hot: true,
+    open: true,
+  },
+
   plugins: [
     //
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('work in progress'),
     }),
+    //
+    new ReactRefreshWebpackPlugin(),
   ],
 };
